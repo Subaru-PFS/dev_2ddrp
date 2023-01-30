fda0fe83654654e71764ec15ca5ffb9e1f0c4312
Hassan Siddiqui <hassans@astro.princeton.edu>
2023-01-29 15:47:54 -1000

    Added latest checks and updates

--- hassan/missingExposures/process.py
+++ hassan/missingExposures/process.py
@@ -1,14 +1,16 @@
+from astropy.io import fits
 import re
 
-regex = ".*PFSA([0-9]{6})([a-z][0-9]).fits"
+regex1 = ".*PFSA([0-9]{6})([a-z][0-9]).fits"
+regex2 = ".*PFSA([0-9]{6})([0-9]{2}).fits"
 
 
 def main():
     ingestedVisitDict = dict()
     problemVisits = set()
-    with open("raw-exposures-hilo.txt", 'r') as f, open("visits_since_nov22.csv", "r") as g, open("missing_exposures-hilo.csv", "w") as h:
+    with open("ingested_exposures_hilo.txt", 'r') as f, open("visits_since_nov22.csv", "r") as g, open("missing_exposures_hilo.csv", "w") as h:
         for filename in f:
-            match = re.search(regex, filename)
+            match = re.search(regex1, filename)
             if match:
                 visit = int(match.group(1))
                 detector = match.group(2)
@@ -26,18 +28,22 @@ def main():
                 continue
             v, arm, spectrograph, p, timeExpStart = line.split(',')
             visit = int(v)
+            if visit < 84574:
+                continue
             pfsDesignId = int(p)
             if visit not in ingestedVisitDict:
-                print(f'visit {visit} not in ingested list.')
+                # print(f'visit {visit} not in ingested list.')
                 continue
             detectors = ingestedVisitDict[visit]
             detector = f'{arm}{spectrograph}'
             if detector not in detectors:
                 h.write(f'{visit}, {arm}, {spectrograph}, {detector}, {hex(pfsDesignId)}, {timeExpStart}')
                 problemVisits.add(visit)
-        # print('Problem visits:')
-        # for v in sorted(problemVisits):
-        #     print(v)
+        print(f'There are {len(problemVisits)} problematic visits.')
+        print(problemVisits)
+        with open('visits-expected.txt', 'w') as h:
+            for v in sorted(problemVisits):
+                h.write(f'{v}\n')
         # Group contiguous visits from problemVisit set
         print('Grouping problem visits:')
         groups = []
@@ -45,8 +51,6 @@ def main():
         endValue = None
         problemVisits = sorted(problemVisits)
         for ii in problemVisits:
-            if ii < 84574:
-                continue
             if not startValue:
                 startValue = ii
                 endValue = ii
@@ -56,8 +60,8 @@ def main():
                 endValue = ii
                 continue
             groups.append((startValue, endValue))
-            startValue = None
-            endValue = None
+            startValue = ii
+            endValue = ii
         groups.append((startValue, endValue))
         print(f'Number of groups: {len(groups)}')
         print(groups)
@@ -65,6 +69,47 @@ def main():
         print("Add this to pfs_utils headerFixes.py:\n")
         for start, end in groups:
             print(f"self.add(\"S\", self.inclRange({start}, {end}), W_PFDSGN=0x5cab8319135e443f)")
+
+        query = "select * from raw where "
+        first = True
+        for start, end in groups:
+            if first:
+                query += f"(visit between {start} and {end})"
+                first = False
+                continue
+            query += f" OR (visit between {start} and {end})"
+
+        query += ";"
+        print(f"Use this to query the registry:\n{query}")
+
+        rawExposuresDict = dict()
+        with open('raw-exposures-hilo.txt', 'r') as j:
+            for file in j:
+                match = re.search(regex2, file)
+                if match:
+                    visit = int(match.group(1))
+                    if not visit in rawExposuresDict:
+                        rawExposuresDict[visit] = []
+                    rawExposuresDict[visit].append(file[:-1])
+
+        with open('problematic-visits-hilo.csv', 'r') as k, open('visitDesign.csv', 'w') as l:
+            for v in k:
+                visit = int(v)
+                print(f'Checking for SuNSS exp for visit {visit}..')
+                if not visit in rawExposuresDict:
+                    print(f'visit {visit} not in rawExposuresDict')
+                    continue
+                foundSuNSS = False
+                foundNonSuNSS = False
+                for file in rawExposuresDict[visit]:
+                    hdul = fits.open(file)
+                    if hdul[0].header['W_PFDSGN'] == 0xdeadbeef:
+                        foundSuNSS = True
+                    else:
+                        foundNonSuNSS = True
+                    l.write(f"{visit} {hex(hdul[0].header['W_PFDSGN'])}, {file}\n")
+                assert(foundSuNSS, f'Visit {visit}: no SuNSS exposure found.')
+                assert(foundNonSuNSS, f'Visit {visit}: no Non-SuNSS exposure found.')
 
 
 if __name__ == "__main__":
